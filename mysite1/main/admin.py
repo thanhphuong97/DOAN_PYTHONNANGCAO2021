13f57dd68d1f3da66214e36d6ec8429fd3a8718a
Thai Son <90693789+ThaiSon09@users.noreply.github.com>
2021-11-11 22:48:02 +0700

    neww

--- mysite1/main/admin.py
+++ mysite1/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Users
 
-# Register your models here.
+admin.site.register(Users)
+UserAdmin.fieldsets += ("Custom fields set", {'fields': ('phone_number', 'birdth_day', 'sex')}),
+
